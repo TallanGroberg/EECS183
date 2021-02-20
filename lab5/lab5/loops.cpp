ada65a88415f448bf561d3f0f545dd429e27bd4e
TallanGroberg <tallan.taven@gmail.com>
2021-02-20 09:32:00 -0500

    finish lab5

--- lab5/lab5/loops.cpp
+++ lab5/lab5/loops.cpp
@@ -13,7 +13,7 @@
 #include "loops.h"
 #include <iostream>
 #include <string>
-
+#include <cmath>
 using namespace std;
 
 /**
@@ -24,12 +24,74 @@ using namespace std;
 
 void printRectangle(int row, int col) {
     
-    string wholeRow;
+
     
     for (int i = 0; i < row; i++) {
-        wholeRow = "";
+        
         for (int j = 0; j < col; j++) {
-            wholeRow += '*';
+            cout << '*';
         }
+        cout << endl;
     }
 }
+
+void printRight(int n) {
+    string wholeRow;
+    
+    for (int i = 0; i < n; i++) {
+        wholeRow += '*';
+        cout << wholeRow;
+        cout << endl;
+    }
+}
+
+void printRightJustified(int n) {
+  
+    
+    int i, j, rows;
+
+     rows = n;
+     for (i = 1; i <= rows; i++)
+     {
+         for (j = 1; j <= rows - i; j++)
+         {
+             cout << " ";
+         }
+
+         for (j = 1; j <= i; j++)
+         {
+             cout << '*';
+             
+         }
+         cout << endl;
+     }
+}
+
+void printIsosceles(int n) {
+    string line = "";
+    
+    // make print 3 by 5
+    
+    //replace all on the close side.
+    int space;
+    int rows = n;
+    int k;
+    
+
+        
+
+        for(int i = 1; i <= rows; ++i) {
+            
+            for(space = 1; space <= rows - i; space++)
+            {
+                cout << " ";
+            }
+
+            for(k = 0; k < 2 * i - 1; k++)
+            {
+                cout << "*";
+            }
+            cout << endl;
+        }
+    
+};
--- lab5/lab5/test.cpp
+++ lab5/lab5/test.cpp
@@ -16,6 +16,9 @@
 using namespace std;
 
 void testPrintRectangle();
+void test_printRight();
+void test_printRightJustified();
+void test_printIsosceles();
 
 int main() {
     
@@ -23,19 +26,87 @@ int main() {
     // Once you have completed the testing functions
     // call them such that the tests actually get run
     testPrintRectangle();
+    test_printRight();
+    test_printRightJustified();
+    test_printIsosceles();
 
     return 0;
 }
 
 void testPrintRectangle() {
     cout << "Testing printRectangle()" << endl;
+    
+    cout << "(8,8): Expected: ********" << endl;
+    cout << "                 ********" << endl;
+    cout << "                 ********" << endl;
+    cout << "                 ********" << endl;
+    cout << "                 ********" << endl;
+    cout << "                 ********" << endl;
+    cout << "                 ********" << endl;
+    cout << "                 ********" << endl;
+    cout << "Actual:        " << endl << endl;
     printRectangle(8, 8);
+    
+    
+    
+    cout << "(1,1): Expected: *" << endl;
+    cout << "         Actual: "; printRectangle(1,1);
+    
+
+    
+    cout << "(2,3): Expected:  ***" << endl;
+    cout << "                  ***" << endl;
+    cout << "         Actual: "  << endl; printRectangle(2,3);
     
 }
 
 // TODO
 // Implement your other test_functions
 
+void test_printRight() {
+    cout << "test_printRight   " << endl << endl;
+    cout << "(3): Expected:  *" << endl;
+    cout << "                **" << endl;
+    cout << "                ***" << endl;
+    cout << "         Actual: "  << endl;
+    printRight(3);
+    
+    
+    cout << "(2): Expected:  *" << endl;
+    cout << "                **" << endl;
+    cout << "       Actual: "  << endl;
+    printRight(2);
+    
+    cout << "(1): Expected:  " << endl;
+    cout << "       Actual: "  << endl;
+    printRight(1);
+    
+    
+}
+
+void test_printRightJustified() {
+    cout << "(1): Expected:  *" << endl;
+    cout << "       Actual:  *"  << endl;
+    printRightJustified(1);
+    
+    cout << "(2): Expected:  *" << endl;
+    cout << "               **"  << endl;
+    cout << "       Actual: "  << endl;
+    printRightJustified(2);
+    
+    cout << "(3): Expected:  *" << endl;
+    cout << "               **"  << endl;
+    cout << "              ***"  << endl;
+    cout << "       Actual: "  << endl;
+    printRightJustified(3);
+}
+
+void test_printIsosceles() {
+    cout << "test_printIsosceles: " << endl;
+
+    
+    printIsosceles(3);
+};
 
 
 
